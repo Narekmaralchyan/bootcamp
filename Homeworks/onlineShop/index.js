65e1346b6139d8f3466a1137b346972ba06e7e7f
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-08-18 14:52:29 +0400

    bugs fixed ,some commments added

--- Homeworks/onlineShop/index.js
+++ Homeworks/onlineShop/index.js
@@ -1,44 +1,37 @@
-import { data } from "./sources.js";
-let cartData = [];
-cartData.price = 0;
+import { data } from "./sources/sources.js"   //Data loaded from www.ebay.com
 const root = document.querySelector(".root")
 const showButton = document.querySelector(".showItems")
 const container = document.querySelector(".items")
 const showCart = document.querySelector(".showCart")
 const searchButton = document.querySelector(".search")
-let cartElement = document.querySelector(".cartElement")
 
-let cartPrice = document.createElement("div")
+let cartElement = document.querySelector(".cartElement")
+let cartPrice = document.createElement("div")    //cartPrice is planned for general price in Cart
 cartPrice.classList.add("cartPriceText")
-    
 let payText = document.createElement("h2")
 payText.innerText = "You must pay---"
-
 let price =document.createElement("div")
-
-
 cartPrice.append(payText , price )
 cartElement.append(cartPrice) 
 
-
-
-
-function fetchData (){
+function fetchData (){                    
     return new Promise((resolve)=>{
         setTimeout(()=>{
             resolve(data)
         },3000)
     })
 }
-
-function createItemCard({name,price,src},index){
-    let card = document.createElement("div")
-    card.classList.add("itemCard")
-    card.id = index
+function createItemCard({name,price,src}){   // createItemCard() is createing <div> element for one object,
+    let card = document.createElement("div")       // card element includes picture,name,price,and button for adding to Cart
+    card.classList.add("itemCard") 
 
     let image = document.createElement("img")
     image.classList.add("itemImage")
+    image.setAttribute("alt","itemPhoto")
     image.setAttribute("src",src)
+   
+    let cont = document.createElement("div") //cont includes price element and add button,
+    cont.classList.add("cont")               //and then card element includes 3 elements for set styles correctly
 
     let itemName = document.createElement("div")
     itemName.classList.add("itemName")
@@ -54,48 +47,44 @@ function createItemCard({name,price,src},index){
     addButton.addEventListener("click",function (event){
         let container = document.createElement("div");
         container.classList.add("itemInCart")
-        container.id = Math.random()
 
         let removeButton = document.createElement("button")
         removeButton.innerText = "REMOVE"
         removeButton.classList.add("removeElem")
         removeButton.addEventListener("click",function(event){
-            cartData = cartData.filter(elem=>elem.id != event.target.parentElement.id)
-            event.target.parentElement.remove()
-            let currentPriceArray = [...document.querySelectorAll(".itemInCart>.itemPrice")].map(elem=>+elem.innerText)
+            event.target.parentElement.parentElement.remove()
+            let currentPriceArray = [...document.querySelectorAll(".itemInCart>.cont>.itemPrice")].map(elem=>+elem.innerText)
             document.querySelector(".currentPrice").innerText = currentPriceArray.reduce((arg,cur)=>arg+cur,0)
             
         })
+        let cont = document.createElement("div")
+        cont.classList.add("cont")
+        cont.append(itemPrice.cloneNode(true),removeButton)
 
-        container.append(image.cloneNode(true),itemName.cloneNode(true),itemPrice.cloneNode(true),removeButton)
-        cartData.push(container);
+        container.append(image.cloneNode(true),itemName.cloneNode(true),cont)
         cartElement.append(container)
 
     })
+    cont.append(itemPrice,addButton)
     
-    
-    card.append(image,itemName , itemPrice ,addButton);
+    card.append(image,itemName , cont);
     return card;
-
 }
-
-function render(){
+function render(){           // render() render all item cards in document
     let loading = document.createElement("h2")
+    loading.classList.add("loading")
     loading.innerText = "LOADING..."
-    container.append(loading)
+    container.append(loading)  
     fetchData()
         .then(data=>JSON.parse(data))
         .then(data=>{
             loading.remove();
-            data.forEach((item,index)=>{
-                container.append(createItemCard(item,index))
+            data.forEach((item,)=>{
+                container.append(createItemCard(item,))
             })
         })
 }
-
-showButton.addEventListener("click",render)
-
-searchButton.addEventListener("input",function(event){
+function search (event){
     let names = document.querySelectorAll(".itemName");
     names.forEach(item=>{
         if(!item.innerText.toLowerCase().includes(searchButton.value.toLowerCase())){
@@ -105,24 +94,25 @@ searchButton.addEventListener("input",function(event){
             item.parentElement.classList.remove("hidden")
         }
     })
-})
+}
+function show(){
+    cartElement.classList.remove("hidden")
+   price.innerText = ""
+    let priceText = document.createElement("h2")
+    priceText.classList.add("itemPrice","currentPrice")
+    let currentPriceArray = [...document.querySelectorAll(".itemInCart>.cont>.itemPrice")].map(elem=>+elem.innerText)
+    priceText.innerText = currentPriceArray.reduce((arg,cur)=>arg+cur,0)
+    price.append(priceText)
+}
 
-showCart.addEventListener("click",function(){
-     cartElement.classList.remove("hidden")
-    price.innerText = ""
-     let priceText = document.createElement("h2")
-     priceText.classList.add("itemPrice","currentPrice")
-     let currentPriceArray = [...document.querySelectorAll(".itemInCart>.itemPrice")].map(elem=>+elem.innerText)
-     priceText.innerText = currentPriceArray.reduce((arg,cur)=>arg+cur,0)
-     price.append(priceText)
- })
+showButton.addEventListener("click",render)
 
+searchButton.addEventListener("input",search)
+
+showCart.addEventListener("click",show)
 
 document.addEventListener("click",function(event){
     if(!event.composedPath().includes(cartElement) && event.target != showCart){
         cartElement.classList.add("hidden")
     }
 })
-
-
-
